68eafb26f758e7e97ad506af5bd591b82b59e857
Sadia0920 <sadiaafrin0920@gmail.com>
2024-12-22 22:53:11 +0600

    Added a banner section to the home page

---
+++ src/components/Banner.jsx
@@ -0,0 +1,25 @@
+import volunteer1 from '../assets/volunteer-1.webp'
+import volunteer2 from '../assets/volunteer-2.webp'
+import volunteer3 from '../assets/volunteer-6.webp'
+import volunteer4 from '../assets/volunteer-8.webp'
+
+export default function Banner() {
+  return (
+  <div className='w-[900px] mx-auto'>
+  <div className="carousel carousel-vertical rounded-box h-[400px]">
+  <div className="carousel-item h-full w-full">
+    <img src={volunteer3} className='w-[900px]'/>
+  </div>
+  <div className="carousel-item h-full w-full">
+    <img src={volunteer1} className='w-[900px]' />
+  </div>
+  <div className="carousel-item h-full w-full">
+    <img src={volunteer2} className='w-[900px]'/>
+  </div>
+  <div className="carousel-item h-full w-full">
+    <img src={volunteer4} className='w-[900px]'/>
+  </div>
+  </div>
+  </div>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import Banner from '../components/Banner'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className='my-7'>
+        <Banner></Banner>
+    </div>
   )
 }
