92910ad77bfb5aca53d30ef93af34bf7efc51f2d
Sadia0920 <sadiaafrin0920@gmail.com>
2024-12-23 16:00:47 +0600

    Added a Volunteer Guide section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from '../components/Banner'
+import VolunteerGuide from '../components/VolunteerGuide'
 
 export default function Home() {
   return (
     <div className='my-7'>
         <Banner></Banner>
+        <VolunteerGuide></VolunteerGuide>
     </div>
   )
 }
