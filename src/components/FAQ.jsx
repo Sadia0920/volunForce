4ff5ea56d748a0d3fb9f19641d8477f88049c453
Sadia0920 <sadiaafrin0920@gmail.com>
2025-04-23 12:12:52 +0600

    change the bg color of faq section

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -5,14 +5,14 @@ export default function FAQ() {
     <div className="w-10/12 mx-auto my-14">
     <h1 className="text-3xl font-bold text-center mb-9">Help & Support Center</h1>
     <h2 className='text-2xl font-medium mb-3'>For Volunteers</h2>
-  <div className="collapse collapse-arrow bg-[rgb(168,224,92,0.2)] mb-3">
+  <div className="collapse collapse-arrow bg-[rgb(168,224,92,0.3)] mb-3">
     <input type="radio" name="my-accordion-2" defaultChecked />
     <div className="collapse-title text-xl font-medium">Can I volunteer remotely?</div>
     <div className="collapse-content">
     <p>Absolutely! We offer both in-person and virtual volunteering opportunities. Use the filters to find options that suit your preferences.</p>
     </div>
   </div>
-  <div className="collapse collapse-arrow bg-[rgb(168,224,92,0.2)] mb-3">
+  <div className="collapse collapse-arrow bg-[rgb(168,224,92,0.3)] mb-3">
     <input type="radio" name="my-accordion-2" />
     <div className="collapse-title text-xl font-medium">What if I can't make it to a scheduled opportunity?</div>
     <div className="collapse-content">
@@ -20,14 +20,14 @@ export default function FAQ() {
     </div>
   </div>
   <h2 className='text-2xl font-medium mb-3 mt-6'>For Organizations</h2>
-  <div className="collapse collapse-arrow bg-[rgb(255,111,0,0.2)] mb-3">
+  <div className="collapse collapse-arrow bg-[rgb(255,111,0,0.3)] mb-3">
     <input type="radio" name="my-accordion-2" />
     <div className="collapse-title text-xl font-medium"> Can I get reports on volunteer performance?</div>
     <div className="collapse-content">
     <p>Yes! The platform provides analytics and reporting tools to track hours, participation rates, and more.</p>
     </div>
   </div>
-  <div className="collapse collapse-arrow bg-[rgb(255,111,0,0.2)]">
+  <div className="collapse collapse-arrow bg-[rgb(255,111,0,0.3)]">
     <input type="radio" name="my-accordion-2" />
     <div className="collapse-title text-xl font-medium"> Can I manage multiple events or campaigns?</div>
     <div className="collapse-content">
